6ab76712087fc5b404508d50c67734c4ef442254
Zack <43650976+zwetekamm@users.noreply.github.com>
2018-12-19 13:41:27 -0600

    Update getValidData.hpp

--- Project4-PredatorPrey/getValidData.hpp
+++ Project4-PredatorPrey/getValidData.hpp
@@ -1,7 +1,4 @@
 /*************************************************************************
- * Program Name: Predator Prey Simulation (Group Project) - getValidData.hpp
- * Names:        Gabriel Cata, Drew Childs, William Conway,
- *               Michael Seehafer, Zachary Wetekamm
  * Date:         July 29, 2018
  * Description:  This is the specification file for the function getValidData().
  *               This function has a parameter of a user variable (does not have
